d77dc022050bb5c43707195cb67c5d006a323c15
Reynaldo Castano <reycastano97@gmail.com>
2023-03-15 16:47:19 -0600

    Cambios añadidos

--- src/components/Components/AddUser.jsx
+++ src/components/Components/AddUser.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 const AddUser = (user) =>{
     axios.post('https://fakestoreapi.com/users', user)
       .then(response => {
-        setUsers([...users, response.data]);
+        setUsers([...user, response.data]);
         console.log('User added successfully');
       })
       .catch(error => console.log(error));
