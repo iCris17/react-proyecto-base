541e1d3c9db31a594b3a6b3b0ff3192c9d0698c6
Karla Villarroel Bastidas <118953410+karlazvb@users.noreply.github.com>
2023-03-15 00:55:21 -0400

    Configuracion inicial (#1)
    * Botones de cateogrias
    
    * estilos css
    
    * estilos para botones
    
    * links necesarios para la tipografia en index.html
    
    * Configuracion Eslint
    
    * Configuracion react router

---
+++ src/components/CategoryFilters/CategoryFilters.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import './styles.css';
+
+const CategoryFilters = () => {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState();
+  const getData = async () => {
+    try {
+      const response = await fetch(`https://fakestoreapi.com/products/categories`);
+
+      if (response.status !== 200) {
+        throw new Error('Not ok');
+      }
+
+      const data = await response.json();
+      setCategories(data);
+    } catch (error) {
+      setError(error);
+    }
+  };
+  useEffect(() => {
+    //SE EJECUTA 1 VEZ CUANDO SE MONTE EL COMPONENTE
+    getData();
+  }, []);
+  if (error) {
+    return <p> error</p>;
+  }
+  return (
+    <div>
+      <div className="button__categories__all">
+        {categories.map((category) => {
+          return (
+            <button className="button" key={category}>
+              {category}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default CategoryFilters;
---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import CategoryFilters from '../CategoryFilters/CategoryFilters';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <CategoryFilters></CategoryFilters>
+      {/* AGREGAR OTROS COMPONENTES AQUI */}
+    </div>
+  );
+};
+export default Dashboard;
