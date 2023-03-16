f851b3b1384459e82c82469c0a8c75a59e5a8918
Karla Villarroel Bastidas <118953410+karlazvb@users.noreply.github.com>
2023-03-16 01:05:23 -0400

    Producto (#3)
    * Botones de cateogrias
    
    * estilos css
    
    * estilos para botones
    
    * links necesarios para la tipografia en index.html
    
    * Configuracion Eslint
    
    * Configuracion react router
    
    * Descripcion de Productos
    
    * Buscador de Ctegorias terminado
    
    * Imagenes de productos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Dashboard from './components/Dashboard/Dashboard';
+
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/category/:categoryName" element={<Dashboard />} />
       </Routes>
     </>
   );
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
 import CategoryFilters from '../CategoryFilters/CategoryFilters';
+import Products from '../Products/Products';
 
 const Dashboard = () => {
   return (
     <div>
       <CategoryFilters></CategoryFilters>
+      <Products></Products>
       {/* AGREGAR OTROS COMPONENTES AQUI */}
     </div>
   );
