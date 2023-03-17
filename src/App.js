6f32066eeffae0e935d0750711bb0d5ec1bfc11c
Reynaldo Castano <reycastano97@gmail.com>
2023-03-16 21:16:16 -0600

    agregando userArea

--- src/App.js
+++ src/App.js
@@ -4,38 +4,16 @@ import Login from './components/Login/Login';
 import { Routes, Route } from 'react-router-dom';
 
 const App = () =>{
-
-  const [showAddUser, setShowAddUser] = useState(false);
-  const [showUpdateUser, setShowUpdateUser] = useState(false);
-  const [showDeleteUser, setShowDeleteUser] = useState(false);
-  const [showGetUser, setShowGetUser] = useState(false);
-
-
-  const handleAddUserClick = () => {
-    setShowAddUser(true);
-  };
-
-  const handleUpdateUserClick = () => {
-    setShowUpdateUser(true);
-  };
-
-  const handleDeleteUserClick = () => {
-    setShowDeleteUser(true);
-  };
-
-  const handleGetUserClick = () => {
-    setShowGetUser(true);
-  };
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/category/:categoryName" element={<LandingPage />} />
         <Route path="/category/:categoryName" element={<ProductsPage />} />
         <Route path="/login" element={<Login/>}/>
       </Routes>
     </>
   );
-
 }
 
 export default App;
