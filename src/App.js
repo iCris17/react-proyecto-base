b6e04e0fb337ab5f266002e5d89bda4a4d5d035c
Cristian Monroy <crismmr09@gmail.com>
2023-03-17 23:59:06 -0600

    Merge conflict solucionado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import LandingPage from "./components/LandingPage/LandingPage";
 import { Routes, Route } from 'react-router-dom';
+import CategoryFilters from "./components/CategoryFilters/CategoryFilters";
     
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/category/:categoryName" element={<LandingPage />} />
+        <Route path="/category/:categoryName" element={<CategoryFilters />} />
       </Routes>
     </>
   );
