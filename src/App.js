9691c5bd1e4aeaf591b46c39a7cf0112a3369ccc
Cristian Monroy <37000434+iCris17@users.noreply.github.com>
2023-03-19 09:10:39 -0600

    Login (#25)
    * Agregado ruteo al área de usuarios
    
    * Eliminados archivos innecesarios
    
    * Excluida parte de usuarios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import ProductsPage from './components/ProductsPage/ProductsPage';
 import LandingPage from './components/LandingPage/LandingPage';
 import Login from './components/Login/Login';
-import UserArea from "./components/UserArea/UserArea"
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/category/:categoryName" element={<ProductsPage />} />
         <Route path="/login" element={<Login/>}/>
-        <Route path="/users" element={<UserArea/>}/>
       </Routes>
     </>
   );
